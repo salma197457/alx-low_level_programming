1cd058d3f2020ab65587ed567b8df2739c3013cb
salma <salma.ali67815@gmail.com>
2023-03-20 08:09:04 -0700

    hey

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main -  prints the alphabet in lowercase
+ *Return: returns 9
+*/
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+	putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
+
