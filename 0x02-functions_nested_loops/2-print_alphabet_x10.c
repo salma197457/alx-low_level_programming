baaedcf99fa255e5239ee15943a0a1a4a351c9c5
salma <salma.ali67815@gmail.com>
2023-03-21 11:06:06 -0700

    hey

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints
+ * Returns: returs 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int o;
+
+	for (o = 1; i <= 10; i++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+
+	_putchar('\n');
+	}
+}
