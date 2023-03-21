2fa733ff7c9406bcebb38b5ff5cb00d1e57c2620
salma <salma.ali67815@gmail.com>
2023-03-21 13:47:45 -0700

    hello

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - checks for lowercase character
+ *@c : int
+ *Return: returns 0
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
