1b3434693be8134a6147c45c36d9c69377a6ce76
salma <salma.ali67815@gmail.com>
2023-03-22 19:02:35 -0700

    hey

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,20 +12,19 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; a <= 9; b++)
-		{
-			for (c = 0; d <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a >= 2 && b >= 4)
-						break;
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(58);
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
+	for (b = 0; a <= 9; b++)
+	{
+	if ((a <= 1 && b >= 9) || (a <= 2 && b <= 3))
+	for (c = 0; d <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + 48);
+	_putchar(b + 48);
+	_putchar(58);
+	_putchar(c + 48);
+	_putchar(d + 48);
+	_putchar('\n');
 				}
 			}
 		}
