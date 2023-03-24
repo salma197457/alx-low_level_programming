9460be654bf603145b6aac7124b16159d3d1420a
salma <salma.ali67815@gmail.com>
2023-03-23 18:48:04 -0700

    hey

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,8 +7,8 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	 for (; n <= 9; n++)
-	 {
+	for (; n <= 9; n++)
+	{
 		if (n == 2 || n == 4)
 		{
 			continue;
@@ -17,6 +17,6 @@ void print_most_numbers(void)
 		{
 			_putchar(n + '0');
 		}
-	 }
+	}
 	_putchar('\n');
 }
