452ee3d371cf383cf73a2b78080954baf827b0c1
salma <salma.ali67815@gmail.com>
2023-04-04 17:17:26 -0700

    hey

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s:parameter
+ * Return:0
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+	x++;
+	}
+	for (x -= 1 ; x >= 0; x--)
+	{
+	_putchar(s[x]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - prints a string, in reverse
+ * @s:parameter
+ * Return:0
+ */
+void rev_string(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+	_putchar(s[x]);
+	x++;
+	}
+	for (x -= 1 ; x >= 0; x--)
+	{
+	_putchar(s[x]);
+	}
+	_putchar('\n');
+}
