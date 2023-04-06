666cebf9a55b9d2c26ea8626ad045978c942f08e
salma <salma.ali67815@gmail.com>
2023-04-05 19:26:20 -0700

    hey

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input
+ * Return: 0
+ */
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+return (quare(n, 1));
+}
+/**
+ * square - fun 2
+ * @n: parameter1
+ * @val: parameter2
+ * Return: 0
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
