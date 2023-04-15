3d652a88c78aeb16dcfc366ce69da68f65c23883
salma <salma.ali67815@gmail.com>
2023-04-14 17:08:49 -0700

    hey

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * string_nconcat - concatenates two strings.
- * @s1: input1
- * @s2: input2
- * @n: input3
+ * _calloc - concatenates two strings
+ * @nmemb: inpuit1
+ * @size: input2
  * Return: 0
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int x, j, k;
-	char *s;
+	char *p;
+	unsigned int i;
 
-	if (s1 == NULL)
-		x = 0;
-	else
-	{
-		for (x = 0; s1[x]; x++)
-	}
-	if (s2 == NULL)
-		j = 0;
-	else
-	{
-		for (j = 0; s2[j]; j++)
-	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (x + j + 1));
-	if (s == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	for (k = 0; k < x; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + x] = s2[k];
-	s[x + j] = '\0';
-	return (s);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
 }
