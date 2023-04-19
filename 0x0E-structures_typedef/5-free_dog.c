896ed1021dd631ce1f8b57698db8d0ba44fb1ee1
salma <salma.ali67815@gmail.com>
2023-04-18 19:36:41 -0700

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include "stdlib.h"
+/**
+ * free_dog- frees dogs
+ * @d:v1
+ * Return:0
+ * */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
